883880709665507f574158a20a9cc6caeb92295d
Administrator <Administrator@SKY-20170728TOW>
2017-11-18 16:01:36 +0800

    update

--- MVC_Test/Index.aspx.cs
+++ MVC_Test/Index.aspx.cs
@@ -16,7 +16,7 @@ namespace MVC_Test
 
         protected void btSubmit_Click(object sender, EventArgs e)
         {
-            this.lbMessage.Text = "hello";
+            this.lbMessage.Text = "hello2";
         }
     }
 }
